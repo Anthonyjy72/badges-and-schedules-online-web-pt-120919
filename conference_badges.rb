6c02c9368e7aab62116c04e36f43326f2291613c
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-18 16:53:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  
+def batch_badge_creator(names)
+  new_array = []
+  names.each do |names|
+    new_array.push (badge_maker(name))
+  end
+  return new_array
 end
